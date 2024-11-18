827c92fd5ff7ed6f05f3ac22c403885372615aaa
t-aono <46856574+t-aono@users.noreply.github.com>
2024-11-18 19:24:02 +0900

    fix: ベースパスを追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,4 +6,5 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [tailwind()],
   site: "https://github.com/t-aono.github.io",
+  base: "/dev-portfolio",
 });
